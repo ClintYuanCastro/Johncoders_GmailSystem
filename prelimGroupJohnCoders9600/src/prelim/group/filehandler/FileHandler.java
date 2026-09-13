bd76842d8653e2d5e819d214d8ec4df3e0fb0c9e
ClintCastro <clintcastro128@gmail.com>
2026-09-13 11:05:56 +0800

    added user class, current user class, and base filehandler(will continue to add)

---
+++ prelimGroupJohnCoders9600/src/prelim/group/filehandler/FileHandler.java
@@ -0,0 +1,28 @@
+package prelim.group.filehandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+import prelim.group.model.Email;
+import prelim.group.userHandling.CurrentUser;
+
+public class FileHandler{
+    ObjectMapper objectMapper;
+    private CurrentUser currentUser;
+    private final File userFolder = new File("userFolder");
+
+    FileHandler(CurrentUser user){
+        this.objectMapper = new ObjectMapper();
+        this.currentUser = user;
+        if (!userFolder.exists()){
+            userFolder.mkdirs();
+        }
+    }
+
+    public void saveMail(Email email) throws IOException {
+        File fileToBeSaved = new File(userFolder, currentUser.toString());
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(fileToBeSaved, email);
+    }
+}
---
+++ prelimGroupJohnCoders9600/src/prelim/group/userHandling/CurrentUser.java
@@ -0,0 +1,29 @@
+package prelim.group.userHandling;
+
+public class CurrentUser {
+    private static User currentUser;
+
+    // Private constructor prevents direct instantiation
+    private CurrentUser() {}
+
+    public static void set(User user) {
+        currentUser = user;
+    }
+
+    public static User get() {
+        return currentUser;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    public static void logout() {
+        currentUser = null;
+    }
+
+    @Override
+    public String toString() {
+        return currentUser != null ? currentUser.getUserID() : "";
+    }
+}
